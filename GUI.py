118761d19b950403637ab66dcf0085cf79a895d1
Tylerlhess <tylerlhess@gmail.com>
2014-06-23 22:44:59 -0600

    Added more boxes

--- GUI.py
+++ GUI.py
@@ -3,33 +3,72 @@ from Tkinter import *
 
 
 
+
 def donothing():
     pass
 
-root = Tk()
-root.title("Books Read Good")
+def buildwindow():
+    root = Tk()
+    root.title("Books Read Good")
 
-menu = Menu(root, tearoff = 0)
-menubar = Menu(root)
-filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="Save", command=donothing)
-filemenu.add_command(label="Close", command=donothing)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=root.quit)
+    menu = Menu(root, tearoff = 0)
+    menubar = Menu(root)
+    filemenu = Menu(menubar, tearoff=0)
+    filemenu.add_command(label="Save", command=donothing)
+    filemenu.add_command(label="Close", command=donothing)
+    filemenu.add_separator()
+    filemenu.add_command(label="Exit", command=root.quit)
 
-menubar.add_cascade(label="File", menu=filemenu)
+    menubar.add_cascade(label="File", menu=filemenu)
 
-helpmenu = Menu(menubar, tearoff=0)
-helpmenu.add_command(label="Help", command=donothing)
-menubar.add_cascade(label="Help", menu=helpmenu)
+    helpmenu = Menu(menubar, tearoff=0)
+    helpmenu.add_command(label="Help", command=donothing)
+    menubar.add_cascade(label="Help", menu=helpmenu)
 
-root.config(menu=menubar)
+    root.config(menu=menubar)
 
+    return root
 
+def adddropdowns(window):
+    global lib
+    Label(window, text="Patron Name").grid(row=1)
+    cust = StringVar()
+    try:
+        cust.set(lib.getPatrons()[0])
+        OptionMenu(window, cust, *lib.getPatrons(), command=donothing()).grid(row=1, column=1)
+    except:
+        cust.set("You have no Patrons")
+        OptionMenu(window, cust, "You have no Patrons", *lib.getPatrons(), command=donothing()).grid(row=1, column=1)
+       
+    Label(window, text="Action").grid(row=2)
+    global act
+    act.set(actions()[1])
+    Button(window, text="-", command=takeAction(act.get())).grid(row=3)
+    
+    OptionMenu(window, act, *actions(), command=namebutton).grid(row=2, column=1)
+    
 
+def namebutton(event):
+    global act, root
+    root.grid_slaves(row=3, column=0)[0]["text"]=act.get()
+    return 
 
+def actions():
+    varlist = []
+    varlist.append("Check In")
+    varlist.append("Check Out")
+    return varlist
 
+def takeAction(stringer):
+    pass
 
+    
 
+lib = library.Library()
+lib.loadCatalog('filename')
+lib.loadPatrons('filename2')
+root = buildwindow()
+act = StringVar()
+adddropdowns(root)
 root.mainloop()
 
--- library.py
+++ library.py
@@ -2,55 +2,61 @@ import item
 import patron
 
 class Library:
-	def __init__(self):
-		self.items = []
-		self.patrons = []
+        def __init__(self):
+                self.items = []
+                self.patrons = []
 
-	def loadCatalog(self, fileName):
-		self.catalogFileName = fileName
-		#Do logic to load catalog here.  Item class is in item.py
+        def loadCatalog(self, fileName):
+                self.catalogFileName = fileName
+                #Do logic to load catalog here.  Item class is in item.py
 
-	def saveCatalog(self):
-		#Logic to save catalog here.  Note that we stored the filename in self.catalogFileName
-		pass
+        def saveCatalog(self):
+                #Logic to save catalog here.  Note that we stored the filename in self.catalogFileName
+                pass
 
-	def loadPatrons(self, fileName):
-		self.patronFileName = fileName
-		#Do logic to load patrons here. 
+        def loadPatrons(self, fileName):
+                self.patronFileName = fileName
+                #Do logic to load patrons here. 
 
-	def incrementDate(self):
-		self.currentDate = self.currentDate + timedelta(days=1)
-		pass
+        def incrementDate(self):
+                self.currentDate = self.currentDate + timedelta(days=1)
+                pass
 
-	def decrementDate(self):
-		self.currentDate = self.currentDate - timedelta(days=1)
-		pass
+        def decrementDate(self):
+                self.currentDate = self.currentDate - timedelta(days=1)
+                pass
 
-	def getItem(self, itemID):
-		for i in self.items:
-			if i.ID == itemID:
-				return i
-		return None
+        def getItem(self, itemID):
+                for i in self.items:
+                        if i.ID == itemID:
+                                return i
+                return None
 
-	def getPatron(self, patronID):
-		for p in self.patrons:
-			if p.ID == patronID:
-				return p
-		return None
+        def getPatron(self, patronID):
+                for p in self.patrons:
+                        if p.ID == patronID:
+                                return p
+                return None
 
-	def getOverdueItems(self):
-		returnList = []
-		for i in self.items:
-			if i.isOverdue():
-				returnList.append(i)
-		return returnList
+        def getPatrons(self):
+                returnList = []
+                for p in self.patrons:
+                        returnList.append(p)
+                return returnList
 
-	def getCheckedOutItems(self):
-		returnList = []
-		for i in self.items:
-			if i.status == CHECKED_OUT:
-				returnList.append(i)
-		return returnList
+        def getOverdueItems(self):
+                returnList = []
+                for i in self.items:
+                        if i.isOverdue():
+                                returnList.append(i)
+                return returnList
+
+        def getCheckedOutItems(self):
+                returnList = []
+                for i in self.items:
+                        if i.status == CHECKED_OUT:
+                                returnList.append(i)
+                return returnList
 
 library = Library()
 library.loadCatalog('filename')
